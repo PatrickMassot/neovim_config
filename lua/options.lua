01ead65378f82066e5b3824856410a8eb06d7105
Patrick Massot <patrickmassot@free.fr>
2024-03-10 16:46:43 -0400

    Option portable

--- lua/options.lua
+++ lua/options.lua
@@ -55,6 +55,9 @@ vim.cmd("let g:vimtex_quickfix_open_on_warning = 0")
 -- Neovide configuration
 
 vim.o.guifont = "JetBrainsMono Nerd Font:h20"
+if vim.fn.hostname() == "portable-pmassot" then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h9"
+end
 if vim.fn.hostname() == "fixe-massy" then
   vim.o.guifont = "JetBrainsMono Nerd Font:h12"
 end
