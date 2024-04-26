7d12ab98d071fdf5845162f3d53dac76ca368490
Patrick Massot <patrickmassot@free.fr>
2024-04-25 22:36:20 -0400

    remove dead code

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -122,12 +122,6 @@ require('lazy').setup({
       {
         'L3MON4D3/LuaSnip',
         build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
-          if vim.fn.has 'win32' == 1 then
-            return
-          end
           return 'make install_jsregexp'
         end)(),
       },
