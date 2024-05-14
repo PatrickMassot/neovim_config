54f099ee9135da8381a335205d66a039c433f2d1
Patrick Massot <patrickmassot@free.fr>
2024-05-14 17:24:28 -0400

    Add code action preview plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -383,7 +383,29 @@ require('lazy').setup({
     config = function()
       require("nvim-tree").setup {}
     end,
-  }
+  },
+  {
+    "aznhe21/actions-preview.nvim",
+    config = function()
+      require("actions-preview").setup {
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        },
+      }
+      vim.keymap.set({ "v", "n" }, "ga", require("actions-preview").code_actions)
+    end,
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
