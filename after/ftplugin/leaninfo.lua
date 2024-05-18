358432cd3bff5dbe128d6fb7a88a4cbeefcd3336
Patrick Massot <patrickmassot@free.fr>
2024-05-18 12:13:42 -0400

    More config for lean

--- after/ftplugin/leaninfo.lua
+++ after/ftplugin/leaninfo.lua
@@ -1,6 +1,11 @@
-vim.bo.textwidth = 100
 vim.opt_local.winfixwidth = false
 vim.opt_local.winfixheight = false
 vim.cmd.wincmd('=')
+vim.bo.textwidth = 100
 
 vim.opt_local.signcolumn = "yes"
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '',
+  command = ":%s/\\s\\+$//e"
+})
