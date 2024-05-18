17cc559a09b12006512e0b2887c245959e064c4b
Patrick Massot <patrickmassot@free.fr>
2024-05-18 12:14:16 -0400

    fireenvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -91,6 +91,16 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
+  { -- neovim in Firefox
+      'glacambre/firenvim',
+
+      -- Lazy load firenvim
+      -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+      lazy = not vim.g.started_by_firenvim,
+      build = function()
+          vim.fn["firenvim#install"](0)
+      end
+  },
 
   -- Next two section are Neorg related.
   {
