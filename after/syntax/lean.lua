c3c054bcb4fe394dc4125dd27f54816f5c23c335
Patrick Massot <patrickmassot@free.fr>
2024-08-09 17:24:59 +0200

    Restore function to close Lean infoview when quitting.

--- after/syntax/lean.lua
+++ after/syntax/lean.lua
@@ -24,8 +24,9 @@ vim.api.nvim_create_autocmd({'BufWinLeave', 'QuitPre'}, {
     local infoview = require('lean.infoview').get_current_infoview()
     if infoview then
       local tab_wins = vim.api.nvim_tabpage_list_wins(0)
-      local lean_wins = vim.tbl_filter(function ()
-        local buf_ft = vim.bo.filetype
+      local lean_wins = vim.tbl_filter(function (w)
+        local buf = vim.api.nvim_win_get_buf(w)
+        local buf_ft = vim.api.nvim_buf_get_option(buf, 'filetype')
         return buf_ft == 'lean'
       end, tab_wins)
       if #lean_wins <= 1 then
