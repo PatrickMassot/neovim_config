c851063cb446b0a2abfb9f47c94ffcda82725fa3
Patrick Massot <patrickmassot@free.fr>
2025-02-17 12:22:23 +0100

    Fix timings

--- lua/options.lua
+++ lua/options.lua
@@ -39,8 +39,8 @@ vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.updatetime = 750
+vim.o.timeoutlen = 750
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
