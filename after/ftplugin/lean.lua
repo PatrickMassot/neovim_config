2dd0e7145463777503ec4cf1a46e3833185bc692
Patrick Massot <patrickmassot@free.fr>
2025-04-18 12:45:53 +0200

    Set conceallevel for lean files

--- after/ftplugin/lean.lua
+++ after/ftplugin/lean.lua
@@ -1,6 +1,7 @@
 -- vim.opt_local.winwidth = 105
 vim.bo.textwidth = 0
 
+vim.opt_local.conceallevel = 0
 
 vim.keymap.set({'n', 'i'}, '<C-.>', function() vim.lsp.buf.code_action({apply=true}) end, { desc = 'Apply first code action' })
 
