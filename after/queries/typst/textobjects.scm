035c6266a87547b3ab43a876f543a1d3bc699ce0
Patrick Massot <patrickmassot@free.fr>
2025-06-02 23:34:55 +0200

    Add a comment explaining treesitter dark magic

--- after/queries/typst/textobjects.scm
+++ after/queries/typst/textobjects.scm
@@ -1,4 +1,4 @@
 (math) @typst_math.outer
 (math (_) @typst_math.inner)
-(code _ @typst_function.outer)
+(code _ @typst_function.outer) ; This is needed for the next query to jump back.
 (call item: (ident) @typst_function.name (_))
