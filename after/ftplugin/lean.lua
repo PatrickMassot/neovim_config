2a527c7c3af3c961ba22316f61bf5e86826104ea
Patrick Massot <patrickmassot@free.fr>
2025-07-19 17:38:34 +0200

    Lean config cleanup

--- after/ftplugin/lean.lua
+++ after/ftplugin/lean.lua
@@ -1,7 +1,13 @@
 -- vim.opt_local.winwidth = 105
-vim.bo.textwidth = 0
+vim.bo.textwidth = 100
 
 vim.opt_local.conceallevel = 0
 
 vim.keymap.set({'n', 'i'}, '<C-.>', function() vim.lsp.buf.code_action({apply=true}) end, { desc = 'Apply first code action' })
 
+vim.opt_local.signcolumn = "yes"
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '',
+  command = ":%s/\\s\\+$//e"
+})
--- after/ftplugin/leaninfo.lua
+++ after/ftplugin/leaninfo.lua
@@ -1,11 +1,3 @@
 vim.opt_local.winfixwidth = false
 vim.opt_local.winfixheight = false
 vim.cmd.wincmd('=')
-vim.bo.textwidth = 100
-
-vim.opt_local.signcolumn = "yes"
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '',
-  command = ":%s/\\s\\+$//e"
-})
