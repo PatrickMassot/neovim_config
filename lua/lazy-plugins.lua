f0626ed2e90327b60119cea515bb32bc60b4c8df
Patrick Massot <patrickmassot@free.fr>
2025-08-03 16:04:25 +0200

    Get rid of auto-auto-complete

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -484,6 +484,11 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        -- Do not show auto-completion automatically, need a keypress
+        menu = {
+          show_on_keyword = false,
+          auto_show = false,
+        },
       },
 
       sources = {
