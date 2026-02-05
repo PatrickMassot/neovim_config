49a822093328dd4246a44c37023ba66d5bdbf457
Patrick Massot <patrickmassot@free.fr>
2026-02-05 18:04:13 +0100

    Better Beamer command
    Remove expected term display

--- lua/user-commands.lua
+++ lua/user-commands.lua
@@ -3,4 +3,5 @@ vim.api.nvim_create_user_command('Beamer',
     vim.cmd("colorscheme vscode")
     vim.cmd("set bg=light")
     vim.cmd("set guifont=DroidSansM_Nerd_Font:b")
+    vim.cmd("lua require 'lean.config'().infoview.view_options.show_term_goals = false")
   end, {nargs = 0})
