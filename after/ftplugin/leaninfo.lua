f73177ca561df957cb3deb70f9ae97da8a932a10
Patrick Massot <patrickmassot@free.fr>
2026-02-11 10:51:03 +0100

    Fix leaninfo keymap

--- after/ftplugin/leaninfo.lua
+++ after/ftplugin/leaninfo.lua
@@ -2,9 +2,9 @@ vim.opt_local.winfixwidth = false
 vim.opt_local.winfixheight = false
 vim.cmd.wincmd('=')
 
-local select_fn = vim.iter(vim.api.nvim_buf_get_keymap(0, 'n')):find(function(maparg) return maparg.lhs == 'gK' end).callback
+local select_fn = vim.iter(vim.api.nvim_buf_get_keymap(0, 'n')):find(function(maparg) return maparg.lhs == 'gK' end)
+		.callback
 vim.api.nvim_buf_set_keymap(
-  0, 'n', 'k', -- or whatever key
-  '', { callback = select_fn })
+	0, 'n', 'k', -- or whatever key
+	'', { callback = select_fn })
 
-vim.keymap.set("n", "n", "<cmd>/• <CR>")
--- lua/user-commands.lua
+++ lua/user-commands.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_user_command('Beamer',
     vim.cmd("set guifont=DroidSansM_Nerd_Font:b")
     vim.cmd("lua require 'lean.config'().infoview.view_options.show_term_goals = false")
   end, {nargs = 0})
+
+local group = vim.api.nvim_create_augroup("lean_autocmd", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "leaninfo" },
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0, 'n', 'n', '<cmd>/• <CR>', { noremap = true, silent = true })
+	end,
+	group = group
+})
