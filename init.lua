dddc88c9b538e8cbd19348eee1ef0484e34857d5
Patrick Massot <patrickmassot@free.fr>
2026-05-08 12:30:08 +0200

    Telescope flex layout

--- init.lua
+++ init.lua
@@ -7,76 +7,76 @@
 
 vim.opt.termguicolors = true
 
-vim.opt.number = false -- line number
-vim.opt.scrolloff = 10 -- keep 10 lines above/below cursor
-vim.opt.sidescrolloff = 10 -- keep 10 columns to left/right of cursor
+vim.opt.number = false                            -- line number
+vim.opt.scrolloff = 10                            -- keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 10                        -- keep 10 columns to left/right of cursor
 
-vim.opt.tabstop = 2 -- tabwidth
-vim.opt.shiftwidth = 2 -- indent width
-vim.opt.softtabstop = 2 -- soft tab stop not tabs on tab/backspace
-vim.opt.expandtab = true -- use spaces instead of tabs
-vim.opt.smartindent = true -- smart auto-indent
-vim.opt.autoindent = true -- copy indent from current line
+vim.opt.tabstop = 2                               -- tabwidth
+vim.opt.shiftwidth = 2                            -- indent width
+vim.opt.softtabstop = 2                           -- soft tab stop not tabs on tab/backspace
+vim.opt.expandtab = true                          -- use spaces instead of tabs
+vim.opt.smartindent = true                        -- smart auto-indent
+vim.opt.autoindent = true                         -- copy indent from current line
 
-vim.opt.ignorecase = true -- case insensitive search
-vim.opt.smartcase = true -- case sensitive if uppercase in string
-vim.opt.hlsearch = true -- highlight search matches
-vim.opt.incsearch = true -- show matches as you type
+vim.opt.ignorecase = true                         -- case insensitive search
+vim.opt.smartcase = true                          -- case sensitive if uppercase in string
+vim.opt.hlsearch = true                           -- highlight search matches
+vim.opt.incsearch = true                          -- show matches as you type
 
-vim.opt.signcolumn = 'yes' -- always show a sign column
+vim.opt.signcolumn = 'yes'                        -- always show a sign column
 -- vim.opt.colorcolumn = "100"                       -- show a column at 100 position chars
-vim.opt.showmatch = true -- highlights matching brackets
-vim.opt.cmdheight = 1 -- single line command line
+vim.opt.showmatch = true                          -- highlights matching brackets
+vim.opt.cmdheight = 1                             -- single line command line
 vim.opt.completeopt = 'menuone,noinsert,noselect' -- completion options
-vim.opt.lazyredraw = true -- do not redraw during macros
-vim.opt.synmaxcol = 300 -- syntax highlighting limit
-vim.opt.fillchars = { eob = ' ' } -- hide "~" on empty lines
-vim.o.textwidth = 80 -- default text width
+vim.opt.lazyredraw = true                         -- do not redraw during macros
+vim.opt.synmaxcol = 300                           -- syntax highlighting limit
+vim.opt.fillchars = { eob = ' ' }                 -- hide "~" on empty lines
+vim.o.textwidth = 80                              -- default text width
 
 local undodir = vim.fn.expand '~/.vim/undodir'
 if
-  vim.fn.isdirectory(undodir) == 0 -- create undodir if nonexistent
+    vim.fn.isdirectory(undodir) == 0 -- create undodir if nonexistent
 then
   vim.fn.mkdir(undodir, 'p')
 end
 
-vim.opt.backup = false -- do not create a backup file
-vim.opt.writebackup = false -- do not write to a backup file
-vim.opt.undofile = true -- do create an undo file
-vim.opt.undodir = undodir -- set the undo directory
-vim.opt.updatetime = 300 -- faster completion
-vim.opt.timeoutlen = 500 -- timeout duration
-vim.opt.ttimeoutlen = 0 -- key code timeout
-vim.opt.autoread = true -- auto-reload changes if outside of neovim
-vim.opt.autowrite = false -- do not auto-save
+vim.opt.backup = false                 -- do not create a backup file
+vim.opt.writebackup = false            -- do not write to a backup file
+vim.opt.undofile = true                -- do create an undo file
+vim.opt.undodir = undodir              -- set the undo directory
+vim.opt.updatetime = 300               -- faster completion
+vim.opt.timeoutlen = 500               -- timeout duration
+vim.opt.ttimeoutlen = 0                -- key code timeout
+vim.opt.autoread = true                -- auto-reload changes if outside of neovim
+vim.opt.autowrite = false              -- do not auto-save
 
-vim.opt.hidden = true -- allow hidden buffers
-vim.opt.errorbells = false -- no error sounds
+vim.opt.hidden = true                  -- allow hidden buffers
+vim.opt.errorbells = false             -- no error sounds
 vim.opt.backspace = 'indent,eol,start' -- better backspace behaviour
-vim.opt.autochdir = false -- do not autochange directories
-vim.opt.iskeyword:append '-' -- include - in words
-vim.opt.path:append '**' -- include subdirs in search
-vim.opt.mouse = 'a' -- enable mouse support
+vim.opt.autochdir = false              -- do not autochange directories
+vim.opt.iskeyword:append '-'           -- include - in words
+vim.opt.path:append '**'               -- include subdirs in search
+vim.opt.mouse = 'a'                    -- enable mouse support
 vim.opt.clipboard:append 'unnamedplus' -- use system clipboard
 
-vim.o.breakindent = true -- enable break indent for wrapped lines
+vim.o.breakindent = true               -- enable break indent for wrapped lines
 
 -- Folding: requires treesitter available at runtime; safe fallback if not
-vim.opt.foldmethod = 'expr' -- use expression for folding
+vim.opt.foldmethod = 'expr'                          -- use expression for folding
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- use treesitter for folding
-vim.opt.foldlevel = 99 -- start with all folds open
+vim.opt.foldlevel = 99                               -- start with all folds open
 
-vim.opt.splitbelow = true -- horizontal splits go below
-vim.opt.splitright = true -- vertical splits go right
+vim.opt.splitbelow = true                            -- horizontal splits go below
+vim.opt.splitright = true                            -- vertical splits go right
 
-vim.opt.wildmenu = true -- tab completion
-vim.opt.wildmode = 'longest:full,full' -- complete longest common match, full completion list, cycle through with Tab
-vim.opt.diffopt:append 'linematch:60' -- improve diff display
-vim.opt.redrawtime = 10000 -- increase neovim redraw tolerance
-vim.opt.maxmempattern = 20000 -- increase max memory
+vim.opt.wildmenu = true                              -- tab completion
+vim.opt.wildmode = 'longest:full,full'               -- complete longest common match, full completion list, cycle through with Tab
+vim.opt.diffopt:append 'linematch:60'                -- improve diff display
+vim.opt.redrawtime = 10000                           -- increase neovim redraw tolerance
+vim.opt.maxmempattern = 20000                        -- increase max memory
 
-vim.o.winborder = 'rounded' -- popups borders (eg. S-k)
-vim.opt.title = true -- Use file name as window title
+vim.o.winborder = 'rounded'                          -- popups borders (eg. S-k)
+vim.opt.title = true                                 -- Use file name as window title
 
 vim.cmd 'let g:vimtex_quickfix_open_on_warning = 0'
 
@@ -133,7 +133,7 @@ vim.diagnostic.config { virtual_text = true } -- use online diagnostics
 -- ============================================================================
 -- KEYMAPS
 -- ============================================================================
-vim.g.mapleader = ' ' -- space for leader
+vim.g.mapleader = ' '      -- space for leader
 vim.g.maplocalleader = ' ' -- space for localleader
 
 -- better movement in wrapped text
@@ -862,7 +862,8 @@ vim.keymap.set('n', '<leader>fG', ':LiveGrepGitRoot<cr>', { desc = '[F]ind by [G
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
 vim.keymap.set('n', '<leader>fj', require('telescope.builtin').jumplist, { desc = '[F]ind [J]ump' })
 vim.keymap.set('n', '<leader>fr', require('telescope.builtin').lsp_references, { desc = '[F]ind [R]eferences' })
-vim.keymap.set('n', '<leader>fa', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[F]ind [A]ll symbols in workspace' })
+vim.keymap.set('n', '<leader>fa', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+  { desc = '[F]ind [A]ll symbols in workspace' })
 -- vim.keymap.set('n', '<leader>fu', require('telescope').extensions.ultisnips.ultisnips, { desc = '[F]ind [U]ltisnips snippets' })
 vim.keymap.set('n', '<leader>fl', require('telescope').extensions.luasnip.luasnip, { desc = '[F]ind [L]uaSnip snippets' })
 vim.keymap.set('n', '<leader>fc', require('telescope').extensions.zoxide.list, { desc = '[F]ind and [Change] directory' })
@@ -901,11 +902,11 @@ require('which-key').setup {
 
   -- Document existing key chains
   spec = {
-    { '<leader>d', group = '[D]iff' },
-    { '<leader>t', group = '[T]oggle' },
-    { '<leader>f', group = '[F]ind using Telescope' },
-    { '<leader>n', group = '[N]otes' },
-    { '<leader>s', group = '[S]plit' },
+    { '<leader>d',  group = '[D]iff' },
+    { '<leader>t',  group = '[T]oggle' },
+    { '<leader>f',  group = '[F]ind using Telescope' },
+    { '<leader>n',  group = '[N]otes' },
+    { '<leader>s',  group = '[S]plit' },
     { '<leader>ol', group = '[Org] [L]ink' },
     { '<leader>oi', group = '[Org] [I]nsert' },
     { '<leader>ox', group = '[Org] Clock' },
@@ -1010,8 +1011,9 @@ vim.api.nvim_create_autocmd('User', {
 })
 
 require('nvim-treesitter')
-  .install({ 'c', 'cpp', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'typst', 'mail' })
-  :wait(300000) -- wait max. 5 minutes
+    .install({ 'c', 'cpp', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'typst',
+      'mail' })
+    :wait(300000) -- wait max. 5 minutes
 
 require('nvim-treesitter-textobjects').setup {
   select = {
