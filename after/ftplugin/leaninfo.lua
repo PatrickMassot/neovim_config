e9e04929b42c3fd318e6beb28b67eeb253069cb1
Patrick Massot <patrickmassot@free.fr>
2026-05-16 22:30:59 +0200

    Remove buggy keymap deletion

--- after/ftplugin/leaninfo.lua
+++ after/ftplugin/leaninfo.lua
@@ -3,5 +3,5 @@ vim.opt_local.winfixheight = false
 vim.cmd.wincmd('=')
 
 
-vim.keymap.del('n', 'gK', { buffer = true })                      -- if you want gK to no longer work
+-- vim.keymap.del('n', 'gK', { buffer = true })                      -- if you want gK to no longer work
 vim.keymap.set('n', 'S', '<Plug>(LeanInfoviewSelect)', { buffer = true })
