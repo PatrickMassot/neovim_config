a0e582b66ee0c3ce022404195f57db96bb8892a0
Patrick Massot <patrickmassot@free.fr>
2026-05-16 16:52:35 +0200

    Restore closing InfoView automatically

--- after/ftplugin/lean.lua
+++ after/ftplugin/lean.lua
@@ -8,10 +8,24 @@ vim.keymap.set({ 'n', 'i' }, '<C-.>', function() vim.lsp.buf.code_action({ apply
 
 vim.opt_local.signcolumn = "yes"
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-	pattern = '',
-	command = ":%s/\\s\\+$//e"
-})
-
 vim.keymap.set({ "n", "i" }, "<C-S-X>", function() vim.cmd("LeanRefreshFileDependencies") end)
+
+vim.api.nvim_create_autocmd({'BufWinLeave', 'QuitPre'}, {
+  group = group,
+  pattern = {'*.lean'},
+  callback = function ()
+    local infoview = require('lean.infoview').get_current_infoview()
+    if infoview then
+      local tab_wins = vim.api.nvim_tabpage_list_wins(0)
+      local lean_wins = vim.tbl_filter(function (w)
+        local buf = vim.api.nvim_win_get_buf(w)
+        local buf_ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+        return buf_ft == 'lean'
+      end, tab_wins)
+      if #lean_wins <= 1 then
+        infoview:close()
+      end
+    end
+  end
+})
 
