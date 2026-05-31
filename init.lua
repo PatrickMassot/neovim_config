baea5e6af156cde9f584812ba9a03fb538d7423a
Patrick Massot <patrickmassot@free.fr>
2026-05-31 16:55:44 +0200

    Forgotten autocmd for talks

--- init.lua
+++ init.lua
@@ -1258,3 +1258,12 @@ end)
 vim.keymap.set('n', '<C-x>', function()
   require('dial.map').manipulate('decrement', 'normal')
 end)
+
+vim.api.nvim_create_user_command('Beamer',
+  function()
+    vim.cmd("colorscheme vscode")
+    vim.cmd("set bg=light")
+    vim.cmd("set guifont=DroidSansM_Nerd_Font:b")
+    vim.cmd("lua require 'lean.config'().infoview.view_options.show_term_goals = false")
+  end, {nargs = 0})
+
