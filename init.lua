3bf424798d535eaf44cf7209d862f1a57873efdc
Patrick Massot <patrickmassot@free.fr>
2026-05-31 16:51:49 +0200

    Forgotten dial config

--- init.lua
+++ init.lua
@@ -4,18 +4,12 @@
 
 -- TODO: récupérer (et éditer) la config de which-key et celle de telescope
 -- regarder config mdd pour inspiration
-vim.lsp.log.set_level("debug")
+vim.lsp.log.set_level 'debug'
 
 local function format_func(level, ...)
   local info = debug.getinfo(2, 'Sl')
-  local header = string.format(
-    '[%s][%s] %s:%s',
-    level,
-    os.date(log_date_format),
-    info.short_src,
-    info.currentline
-  )
-  local parts = { }
+  local header = string.format('[%s][%s] %s:%s', level, os.date(log_date_format), info.short_src, info.currentline)
+  local parts = {}
   local argc = select('#', ...)
   for i = 1, argc do
     local arg = select(i, ...)
@@ -1239,3 +1233,28 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
+local augend = require 'dial.augend'
+require('dial.config').augends:register_group {
+  default = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+
+    augend.constant.new {
+      elements = { '₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉' },
+      word = false,
+      cyclic = true,
+    },
+    augend.constant.new {
+      elements = { '⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹' },
+      word = false,
+      cyclic = true,
+    },
+  },
+}
+vim.keymap.set('n', '<C-a>', function()
+  require('dial.map').manipulate('increment', 'normal')
+end)
+vim.keymap.set('n', '<C-x>', function()
+  require('dial.map').manipulate('decrement', 'normal')
+end)
